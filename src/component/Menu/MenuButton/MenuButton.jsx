c82110f49c99edf146551aab01d1ab5746407be8
Kamil Ertekin <48670775+Tasarimoloji@users.noreply.github.com>
2022-04-03 15:07:00 +0200

    Added the animation for the cross icon for menu

--- src/component/Menu/MenuButton/MenuButton.jsx
+++ src/component/Menu/MenuButton/MenuButton.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react"
 import { MenuContext } from "../MenuManager/MenuManager"
 import "./MenuButton.scss"
-
+import cn from "classnames"
 function MenuButton() {
   const { toggle, setToggle } = useContext(MenuContext)
 
   return (
-    <div className="MenuButton">
+    //checks if it is opened
+    <div className={cn("MenuButton", { toggle })}>
       <button className="MenuButton__button" onClick={() => setToggle(!toggle)}>
         <span></span>
       </button>
